7c9d3d6d26b64d351ae4ea59c9aee8e2536c9f9c
Lee Zamastil <elzed@users.noreply.github.com>
2022-02-24 11:09:57 -0800

    Clean up TOOOs

--- app.js
+++ app.js
@@ -1,16 +1,4 @@
-/*
-let form = document.querySelector('form');
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let data = new FormData(form)
-    for (let [name, value] of data) {
-        console.log(data);
-    }
-});
-*/
-
-// let learnerLevel = prompt('Enter Learner Level\n1 for Beginner, 2 for Intermediate, 3 for Advanced');
+// TODO: let learnerLevel = prompt('Enter Learner Level\n1 for Beginner, 2 for Intermediate, 3 for Advanced');
 
 
 const WORK_TO_LEARN_RATIO = 2.5;
@@ -31,23 +19,13 @@ let studyHoursInt = Math.floor(studyCodeHoursDec);
 let studyMinsInt = Math.round(((studyCodeHoursDec * 60) % 60));
 
 
-
-
 console.log(`${totalHours/2} hours shall be spent on freelancing proposals and job applications.`);
 console.log(`Your remaining time of ${totalHours/2} hours of labor will be as follows:`);
 console.log(`- Study for ${studyHoursInt} hours and ${studyMinsInt} minutes \n- Code for ${devCodeHoursInt} hours and ${devCodeMinsInt} minutes`);
 
-// 2.85 into hours and minutes
-// 2.85
-
-// TODO: Average out time to put tasks into pomodoros
 console.log(`\nOr, get focused and follow the pomodoros...`);
 console.log(`Spend ${( totalHours/2 * 60 ) / 30} pomodoros on proposals and applications.`);
 
-// If studyCodeHoursDec to the right of the decimal is > .25 && < .75 then round to an odd # of pomodoros
-// Else round to an even # of pomodoros
-
-// New Pomodoro Branch:
 
 let studyPomodoros = function() {
     let studyPomsHours = (studyHoursInt * 2);
@@ -63,7 +41,6 @@ let studyPomodoros = function() {
 
     return studyPomsHours + studyPomsMins;
 }
-
 console.log(`Spend ${studyPomodoros()} pomodoros on studying.`);
 
 
@@ -81,5 +58,4 @@ let devCodePomodoros = function() {
 
     return devCodePomsHours + devCodePomsMins;
 }
-
 console.log(`Spend ${devCodePomodoros()} pomodoros on coding.`);
